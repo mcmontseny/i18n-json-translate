12f7f00230a1f743e80419d924bf264a977ba309
mcmontseny <mcmontseny.dev@gmail.com>
2022-01-31 00:44:57 +0100

    Using API + basic checkers before translate JSON Obj

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from "./components/Footer";
 import TranslateBox from "./components/TranslateBox";
 
 function App() {
+  const API_USERNAME = process.env.REACT_APP_API_USERNAME
+  const API_PASSWORD = process.env.REACT_APP_API_PASSWORD;
+  
   const langs = [
     { key: "Spanish", value: "es" },
     { key: "English", value: "en" },
@@ -20,11 +23,69 @@ function App() {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
 
+  const translateJSON = async () => {
+    if (!inputText) {
+      alert('Source text cannot be empty')
+      return;
+    }
+
+    if (!chkTextIsValid()) {
+      alert('Source text is not a valid JSON Object');
+      return;
+    }
+
+    const translatedObject = JSON.parse(inputText);
+
+    for (const [key, value] of Object.entries(translatedObject)) {
+      translatedObject[key] = await getAPITranslation(value);
+    }
+
+    setTranslation(translatedObject);
+  };
+
+  const chkTextIsValid = () => {
+    try {
+      JSON.parse(inputText);
+      return true;
+    } catch (e) { return false; }
+  };
+
+  const getAPITranslation = (word) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const API_TRANSLATE_URL = "https://frengly.com/frengly/data/translateREST";
+        const headers = { 'Content-Type': 'application/json' };
+        const body = {
+          "email": API_USERNAME,
+          "password": API_PASSWORD,
+          "text": word,
+          "src": fromLanguage.value,
+          "dest": toLanguage.value
+        };
+
+        const response = await fetch(API_TRANSLATE_URL, {method: "POST", body: JSON.stringify(body), headers})
+          .then((response) => response.json())
+          .catch((err) => {
+            console.log("Error Failed to get translation", err);
+            throw err;
+          });
+
+        resolve(response.translation);
+      } catch (error) {
+        reject({ err: error });
+      }
+    });
+  }
+
+  const setTranslation = (translatedJSON) => setOutputText(JSON.stringify(translatedJSON));
+
+
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <div className="flex flex-col space-y-10 flex-1 bg-white p-6 py-10 md:px-20 lg:px-44">
-        
+
         <div className="flex flex-col space-y-10 justify-around xl:flex-row  xl:space-y-0  xl:space-x-32 flex-1">
           <TranslateBox
             title="Translate from:"
@@ -46,7 +107,7 @@ function App() {
           />
         </div>
         <div className="text-center">
-          <button className="bg-blue-500 text-white font-bold p-3 px-10 rounded-full shadow-md">
+          <button onClick={() => translateJSON()} className="bg-blue-500 text-white font-bold p-3 px-10 rounded-full shadow-md">
             🪄 Translate JSON File
           </button>
         </div>
